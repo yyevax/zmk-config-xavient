fbf1f1d72403b1f7b1eeb3d9a6ec462597b53f21
yyevax <dxbondad06@gmail.com>
2026-04-20 23:17:34 +0800

    Update keypos_42keys.h

--- config/keypos_def/keypos_42keys.h
+++ config/keypos_def/keypos_42keys.h
@@ -90,12 +90,12 @@
 #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumb keys
 
 #define LAYER_FROM42( \
-   k36, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k39, \
-   k37, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k40, \
-   k38, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k41, \
-              k30, k31, k32, k33, k34, k35            \
+    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, \
+    k12, k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, \
+    k24, k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, \
+                  k36, k37, k38, k39, k40, k41                 \
 ) \
-    k36  k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  k39 \
-    k37  k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  k40 \
-    k38  k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  k41 \
-                   k30  k31  k32  k33  k34  k35
+    k00 k01 k02 k03 k04 k05 k06 k07 k08 k09 k10 k11 \
+    k12 k13 k14 k15 k16 k17 k18 k19 k20 k21 k22 k23 \
+    k24 k25 k26 k27 k28 k29 k30 k31 k32 k33 k34 k35 \
+                k36 k37 k38 k39 k40 k41
