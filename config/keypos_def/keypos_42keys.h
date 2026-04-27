44c80a7ac5d12951140af5fe5ac02b289ea3c90c
yyevax <dxbondad06@gmail.com>
2026-04-27 14:52:17 +0800

    Update keypos_42keys.h

--- config/keypos_def/keypos_42keys.h
+++ config/keypos_def/keypos_42keys.h
@@ -35,70 +35,67 @@
 
 #pragma once
 
-// Left-hand keys (0-5, 12-17, 24-29)
-#define LT5 0   // ESC
-#define LT4 1   // Q
-#define LT3 2   // W
-#define LT2 3   // F
-#define LT1 4   // P
-#define LT0 5   // B
+#define LT0  4  // left-top row
+#define LT1  3
+#define LT2  2
+#define LT3  1
+#define LT4  0
+#define LT5  36
 
-#define LM5 12  // LSFT
-#define LM4 13  // A
-#define LM3 14  // R
-#define LM2 15  // S
-#define LM1 16  // T
-#define LM0 17  // G
+#define RT0  5  // right-top row
+#define RT1  6
+#define RT2  7
+#define RT3  8
+#define RT4  9
+#define RT5  39
 
-#define LB5 24  // LCTRL
-#define LB4 25  // Z
-#define LB3 26  // X
-#define LB2 27  // C
-#define LB1 28  // D
-#define LB0 29  // V
+#define LM0 14  // left-middle row
+#define LM1 13
+#define LM2 12
+#define LM3 11
+#define LM4 10
+#define LM5 37
 
-// Right-hand keys (6-11, 18-23, 30-35)
-#define RT0 6   // J
-#define RT1 7   // L
-#define RT2 8   // U
-#define RT3 9   // Y
-#define RT4 10  // SEMI
-#define RT5 11  // BSPC
+#define RM0 15  // right-middle row
+#define RM1 16
+#define RM2 17
+#define RM3 18
+#define RM4 19
+#define RM5 40
 
-#define RM0 18  // M
-#define RM1 19  // N
-#define RM2 20  // E
-#define RM3 21  // I
-#define RM4 22  // O
-#define RM5 23  // RET
+#define LB0 24  // left-bottom row
+#define LB1 23
+#define LB2 22
+#define LB3 21
+#define LB4 20
+#define LB5 38
 
-#define RB0 30  // K
-#define RB1 31  // H
-#define RB2 32  // COMMA
-#define RB3 33  // DOT
-#define RB4 34  // FSLH
-#define RB5 35  // DEL
+#define RB0 25  // right-bottom row
+#define RB1 26
+#define RB2 27
+#define RB3 28
+#define RB4 29
+#define RB5 41
 
-// Thumb keys (36-41)
-#define LH2 36  
-#define LH1 37 
-#define LH0 38 
+#define LH0 32  // left thumb keys
+#define LH1 31
+#define LH2 30
 
-#define RH0 39  
-#define RH1 40  
-#define RH2 41
+#define RH0 33  // right thumb keys
+#define RH1 34
+#define RH2 35
 
-#define KEYS_L LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5 // left-hand keys
-#define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5 // right-hand keys
+#define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4  // left-hand keys
+#define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4  // right-hand keys
 #define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                      // thumb keys
 
 #define LAYER_FROM42( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k10, k11, \
-    k12, k13, k14, k15, k16, k17, k18, k19, k20, k21, k22, k23, \
-    k24, k25, k26, k27, k28, k29, k30, k31, k32, k33, k34, k35, \
-                  k36, k37, k38, k39, k40, k41                 \
+   k36, k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, k39, \
+   k37, k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, k40, \
+   k38, k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, k41, \
+              k30, k31, k32, k33, k34, k35            \
 ) \
-    k00 k01 k02 k03 k04 k05 k06 k07 k08 k09 k10 k11 \
-    k12 k13 k14 k15 k16 k17 k18 k19 k20 k21 k22 k23 \
-    k24 k25 k26 k27 k28 k29 k30 k31 k32 k33 k34 k35 \
-                k36 k37 k38 k39 k40 k41
+    k36  k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  k39 \
+    k37  k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  k40 \
+    k38  k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  k41 \
+                   k30  k31  k32  k33  k34  k35
