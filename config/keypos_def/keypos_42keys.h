0b7cab6dd19cb0cea5153f33c210175b6fb5e825
yyevax <dxbondad06@gmail.com>
2026-04-27 16:39:16 +0800

    Update keypos_42keys.h

--- config/keypos_def/keypos_42keys.h
+++ config/keypos_def/keypos_42keys.h
@@ -24,66 +24,74 @@
  * SOFTWARE.
  */
 
-/*                              36 KEY MATRIX / LAYOUT MAPPING
+/*                              /* 42 KEY MATRIX / LAYOUT MAPPING
 
-  ╭───────────────────────┬────────────────────────╮ ╭─────────────────────────┬─────────────────────────╮
-  │ 36  0   1   2   3   4 │  5   6   7   8   9 39  │ │ LT5 LT4 LT3 LT2 LT1 LT0 │ RT0 RT1 RT2 RT3 RT4 RT5 │
-  │ 37 10  11  12  13  14 │ 15  16  17  18  19 40  │ │ LM5 LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 RM5 │
-  │ 38 20  21  22  23  24 │ 25  26  27  28  29 41  │ │ LB5 LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 RB5 │
-  ╰──────────╮ 30  31  32 │ 33  34  35 ╭───────────╯ ╰───────────╮ LH2 LH1 LH0 │ RH0 RH1 RH2 ╭───────────╯
-             ╰────────────┴────────────╯                         ╰─────────────┴─────────────╯             */
+   ╭───────────────────────────────╮   ╭───────────────────────────────╮
+   │  0   1   2   3   4   5        │   │  6   7   8   9  10  11        │
+   │ LT5 LT4 LT3 LT2 LT1 LT0       │   │ RT0 RT1 RT2 RT3 RT4 RT5       │
+   │                               │   │                               │
+   │ 12  13  14  15  16  17        │   │ 18  19  20  21  22  23        │
+   │ LM5 LM4 LM3 LM2 LM1 LM0       │   │ RM0 RM1 RM2 RM3 RM4 RM5       │
+   │                               │   │                               │
+   │ 24  25  26  27  28  29        │   │ 30  31  32  33  34  35        │
+   │ LB5 LB4 LB3 LB2 LB1 LB0       │   │ RB0 RB1 RB2 RB3 RB4 RB5       │
+   │                               │   │                               │
+   ╰──────────────╮ 36  37  38     │   │ 39  40  41     ╭──────────────╯
+                  │ LH2 LH1 LH0    │   │ RH0 RH1 RH2    │
+                  ╰────────────────╯   ╰────────────────╯
+*/
 
 #pragma once
 
-#define LT0  4  // left-top row
-#define LT1  3
-#define LT2  2
-#define LT3  1
-#define LT4  0
-#define LT5  36
+#define LT0 5
+#define LT1 4
+#define LT2 3
+#define LT3 2
+#define LT4 1
+#define LT5 0
 
-#define RT0  5  // right-top row
-#define RT1  6
-#define RT2  7
-#define RT3  8
-#define RT4  9
-#define RT5  39
+#define RT0 6
+#define RT1 7
+#define RT2 8
+#define RT3 9
+#define RT4 10
+#define RT5 11
 
-#define LM0 14  // left-middle row
-#define LM1 13
-#define LM2 12
-#define LM3 11
-#define LM4 10
-#define LM5 37
+#define LM0 17
+#define LM1 16
+#define LM2 15
+#define LM3 14
+#define LM4 13
+#define LM5 12
 
-#define RM0 15  // right-middle row
-#define RM1 16
-#define RM2 17
-#define RM3 18
-#define RM4 19
-#define RM5 40
+#define RM0 18
+#define RM1 19
+#define RM2 20
+#define RM3 21
+#define RM4 22
+#define RM5 23
 
-#define LB0 24  // left-bottom row
-#define LB1 23
-#define LB2 22
-#define LB3 21
-#define LB4 20
-#define LB5 38
+#define LB0 29
+#define LB1 28
+#define LB2 27
+#define LB3 26
+#define LB4 25
+#define LB5 24
 
-#define RB0 25  // right-bottom row
-#define RB1 26
-#define RB2 27
-#define RB3 28
-#define RB4 29
-#define RB5 41
+#define RB0 30
+#define RB1 31
+#define RB2 32
+#define RB3 33
+#define RB4 34
+#define RB5 35
 
-#define LH0 32  // left thumb keys
-#define LH1 31
-#define LH2 30
+#define LH0 38
+#define LH1 37
+#define LH2 36
 
-#define RH0 33  // right thumb keys
-#define RH1 34
-#define RH2 35
+#define RH0 39
+#define RH1 40
+#define RH2 41
 
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LT5 LM0 LM1 LM2 LM3 LM4 LM5 LB0 LB1 LB2 LB3 LB4 LB5 // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RT5 RM0 RM1 RM2 RM3 RM4 RM5 RB0 RB1 RB2 RB3 RB4 RB5 // right-hand keys
